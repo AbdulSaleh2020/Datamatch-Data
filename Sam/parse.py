0289b9866d3e7a27af933c8bd71a987c39744e27
Sam Goldman <samgoldman97@gmail.com>
2017-01-03 11:36:04 -0500

    A network shows up now.  It's still painfully slow and doesn't seem to stabilize, but it exists. Need to play with physics of it and apperance

--- Sam/parse.py
+++ Sam/parse.py
@@ -30,10 +30,13 @@ for nodeIndex, line in enumerate(lines):
 	indexDict[Id] = {}
 	indexDict[Id]['matches'] = matches
 	indexDict[Id]['index'] = nodeIndex
-	matchesEntered[Id] = {}
+	# create an array to hold all the connections that have been added
+	matchesEntered[Id] = []
 	
-# Once we've finished building this indexDict, lop through to build edges
+# Once we've finished building this indexDict, loop through to build edges
 for key in indexDict:
+	# Only take the first 3 matches
+	#matches = indexDict[key]['matches'][0:3]
 	matches = indexDict[key]['matches']
 	sourceIndex = indexDict[key]['index']
 	for match in matches:
@@ -44,16 +47,16 @@ for key in indexDict:
 		target, q1, q2, q3 = match.split(",")
 		sourceNode = key
 		targetNode = target
-		if sourceNode not in matchesEntered[targetNode]:
-			# Get the target's key
-			if target in indexDict.keys():
-				targetIndex = indexDict[target]['index']
-				connection_dict = {'source':int(sourceIndex), 'target':int(targetIndex), 'eligible':q1, 'interested':q2, 'reverse interest':q3}
-				edges.append(connection_dict)
+		if targetNode in matchesEntered.keys():
+			# Filter out edges we've already tracked
+			if sourceNode not in matchesEntered[targetNode]:
+				# Keep track of this so we can filter in the future
+				matchesEntered[sourceNode].append(targetNode)
 
+				# Get the target's key
+				if target in indexDict.keys():
+					targetIndex = indexDict[target]['index']
+					connection_dict = {'source':int(sourceIndex), 'target':int(targetIndex), 'eligible':q1, 'interested':q2, 'reverse interest':q3}
+					edges.append(connection_dict)
 myOutput = {'nodes':nodes, 'edges':edges}
 print(json.dumps(myOutput))
-
-
-
-### FIX MATCHES ENTERED CODE TO REDUCE DUPLICATE EDGES 
